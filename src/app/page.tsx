ce5b1ea3df6b393c649f6f22a6cafdf163a1c6bc
munyasya21 <munyasyacynthia@gmail.com>
2023-10-17 13:41:17 +0300

    website

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+export default function Home() {
+  return (
+   
+       <section>
+          <div>
+        <header>
+          <h1 className=" flex-1 text-2xl font-bold  mt-9 ">
+            TOREX
+          </h1>
+          </header>
+          <div>
+            <h2 className=" font-extrabold text-[90px] mt-[200px]  ml-[110px]  leading-[100px]  tracking-[-1.6px]">
+              A Class Of 
+              <br/>
+              Your Own
+            </h2>
+            <div>
+              <p className=" font-medium ml-[110px] text-[22px] ">
+                Where Elegance Is Obtained.
+              </p>
+            </div>
+          </div>
+            
+        </div>
+        <section>
+          <div>
+
+          </div>
+        </section>
+       </section>
+
+       
+     
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return <>
+    <header>
+        <h1 className=" text-2xl font-bold">
+          SHELLY SHOP
+        </h1>
+    </header>
+    </>
+}
