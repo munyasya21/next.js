6cd7f54e9fbb63519c37aa51d6ea4d46af9a69af
munyasya21 <munyasyacynthia@gmail.com>
2023-10-17 15:21:50 +0300

    website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,23 @@ export default function Home() {
        <section>
           <div>
         <header>
-          <h1 className=" flex-1 text-2xl font-bold  mt-9 ">
+          <div className="flex-1 text-2xl font-bold mt-9 justify-between w-full  inline-flex items-center border-b-2 border-transparent px-1 pt-1 hover:border-gray-300 hover:text-gray-700  ">
+          <h1>
             TOREX
           </h1>
+          <div>
+            Contact Us
+          </div>
+          <div>
+            About Us
+          </div>
+          <div>
+            Log In
+          </div>
+          </div>
           </header>
           <div>
-            <h2 className=" font-extrabold text-[90px] mt-[200px]  ml-[110px]  leading-[100px]  tracking-[-1.6px]">
+            <h2 className=" font-extrabold text-[90px] mt-[200px] ml-[110px] leading-[100px] tracking-[-1.6px]">
               A Class Of 
               <br/>
               Your Own
@@ -20,6 +31,15 @@ export default function Home() {
               </p>
             </div>
           </div>
+          <div className="flex ml-[110px] mr-[16.52px] h-[50px] my-2  ">
+              <button className=" px-3 py-3 rounded-xl border border-[bg-Color-14] shadow-sm  bg-Foundation   whitespace-nowrap text-Color-18 font-medium text-base tracking-[-0.2px]">
+                Log in
+              </button>
+
+              <button className="flex items-center justify-center ml-[17px] mr-[16.1px]  whitespace-nowrap self-stretch px-3 py-3 rounded-xl bg-Color-5  border border-[bg-Color-17] ">
+                Talk To Us
+              </button>
+            </div>
             
         </div>
         <section>
